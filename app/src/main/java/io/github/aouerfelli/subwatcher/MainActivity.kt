2349b955bd0dd9de77876a4125605ef367ba9695
aouerfelli <a.ouerf@gmail.com>
2019-09-18 14:20:20 -0400

    Setup view binding

--- app/src/main/java/io/github/aouerfelli/subwatcher/MainActivity.kt
+++ app/src/main/java/io/github/aouerfelli/subwatcher/MainActivity.kt
@@ -2,11 +2,15 @@ package io.github.aouerfelli.subwatcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.github.aouerfelli.subwatcher.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
