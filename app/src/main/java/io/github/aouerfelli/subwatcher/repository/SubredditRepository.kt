2ef74ab3bf415cc6f139f20193c59f87dccf4afc
aouerfelli <a.ouerf@gmail.com>
2019-10-03 11:31:31 -0400

    Changed in-memory subreddit list type from a set to a map

--- app/src/main/java/io/github/aouerfelli/subwatcher/repository/SubredditRepository.kt
+++ app/src/main/java/io/github/aouerfelli/subwatcher/repository/SubredditRepository.kt
@@ -16,8 +16,8 @@ import javax.inject.Singleton
 @Singleton
 class SubredditRepository @Inject constructor(private val service: RedditService) {
 
-    private val _subreddits = ConflatedBroadcastChannel<Set<Subreddit>>(emptySet())
-    val subreddits = _subreddits.asFlow().mapLatest { it.toList() }
+    private val _subreddits = ConflatedBroadcastChannel<Map<SubredditName, Subreddit>>(emptyMap())
+    val subreddits = _subreddits.asFlow().mapLatest { it.values.toList() }
 
     private suspend fun fetchSubreddit(subredditName: SubredditName): Subreddit {
         val aboutSubreddit = service.getAboutSubreddit(subredditName.value).data
@@ -31,21 +31,21 @@ class SubredditRepository @Inject constructor(private val service: RedditService
     suspend fun addSubreddit(subredditName: String): Subreddit {
         val newSubredditName = SubredditName(subredditName)
 
-        val duplicateSubreddit = _subreddits.value.firstOrNull { it.name == newSubredditName }
+        val duplicateSubreddit = _subreddits.value[newSubredditName]
         if (duplicateSubreddit != null) {
             return duplicateSubreddit
         }
 
         val newSubreddit = fetchSubreddit(newSubredditName)
-        _subreddits.send(_subreddits.value + newSubreddit)
+        _subreddits.send(_subreddits.value + (newSubreddit.name to newSubreddit))
         return newSubreddit
     }
 
     suspend fun refreshSubreddits() {
         coroutineScope {
-            val newSubreddits = _subreddits.value.map { subreddit ->
+            val newSubreddits = _subreddits.value.values.map { subreddit ->
                 async(context = Dispatchers.IO) { fetchSubreddit(subreddit.name) }
-            }.awaitAll().toSet()
+            }.awaitAll().associateBy { it.name }
             _subreddits.send(newSubreddits)
         }
     }
