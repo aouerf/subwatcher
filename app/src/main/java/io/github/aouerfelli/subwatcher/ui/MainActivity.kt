5369b140f8545d18ec2122e8e95aab8dc63a2c32
aouerfelli <a.ouerf@gmail.com>
2019-10-09 13:41:40 -0400

    Used FragmentContainerView instead of FrameLayout in MainActivity and set the initial fragment in the layout

--- app/src/main/java/io/github/aouerfelli/subwatcher/ui/MainActivity.kt
+++ app/src/main/java/io/github/aouerfelli/subwatcher/ui/MainActivity.kt
@@ -2,20 +2,12 @@ package io.github.aouerfelli.subwatcher.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.commitNow
-import androidx.fragment.app.replace
 import io.github.aouerfelli.subwatcher.R
-import io.github.aouerfelli.subwatcher.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.commitNow {
-                replace<MainFragment>(R.id.container)
-            }
-        }
     }
 }
