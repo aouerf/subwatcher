e2fc9c576c75f8a12b9a97216a8d738914bb3024
aouerfelli <a.ouerf@gmail.com>
2019-10-13 21:49:18 -0400

    Simplified MainActivity

--- app/src/main/java/io/github/aouerfelli/subwatcher/ui/MainActivity.kt
+++ app/src/main/java/io/github/aouerfelli/subwatcher/ui/MainActivity.kt
@@ -1,13 +1,6 @@
 package io.github.aouerfelli.subwatcher.ui
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.github.aouerfelli.subwatcher.R
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.main_activity)
