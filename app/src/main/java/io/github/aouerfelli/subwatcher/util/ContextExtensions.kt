c00c67cd090babba609c27d92217659751784122
aouerfelli <a.ouerf@gmail.com>
2019-10-25 19:59:43 -0400

    Update Context.getThemeColor extension function to be more idiomatic

--- app/src/main/java/io/github/aouerfelli/subwatcher/util/ContextExtensions.kt
+++ app/src/main/java/io/github/aouerfelli/subwatcher/util/ContextExtensions.kt
@@ -1,21 +1,18 @@
 package io.github.aouerfelli.subwatcher.util
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
 import android.view.LayoutInflater
 import androidx.annotation.AttrRes
 import androidx.annotation.ColorInt
+import androidx.core.content.res.use
 
 inline val Context.layoutInflater: LayoutInflater
     get() = LayoutInflater.from(this)
 
+@SuppressLint("Recycle")
 @ColorInt
 fun Context.getThemeColor(@AttrRes attrResId: Int, @ColorInt defaultValue: Int = Color.BLACK): Int {
-    return with(obtainStyledAttributes(null, intArrayOf(attrResId))) {
-        try {
-            getColor(0, defaultValue)
-        } finally {
-            recycle()
-        }
-    }
+    return obtainStyledAttributes(null, intArrayOf(attrResId)).use { it.getColor(0, defaultValue) }
 }
