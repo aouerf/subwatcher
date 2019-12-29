250789eccdc7cce507081209ed7a9efc322086f9
aouerfelli <a.ouerf@gmail.com>
2019-12-29 12:59:51 -0500

    Add cache to OkHttp client and perform lazy initialization of OkHttp client

--- app/src/main/java/io/github/aouerfelli/subwatcher/network/NetworkModule.kt
+++ app/src/main/java/io/github/aouerfelli/subwatcher/network/NetworkModule.kt
@@ -1,9 +1,15 @@
 package io.github.aouerfelli.subwatcher.network
 
+import android.content.Context
+import dagger.Lazy
 import dagger.Module
 import dagger.Provides
+import javax.inject.Qualifier
 import javax.inject.Singleton
+import okhttp3.Cache
+import okhttp3.Call
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -17,23 +23,44 @@ object NetworkModule {
     const val BASE_URL = "https://www.reddit.com"
     private const val LOG_TAG = "OkHttp"
 
+    @Retention(AnnotationRetention.BINARY)
+    @Qualifier
+    private annotation class InternalApi
+
     @Provides
+    @InternalApi
+    fun provideCache(context: Context): Cache {
+        val cacheSize = 10L * 1024 * 1024 // 10 MiB
+        return Cache(context.cacheDir, cacheSize)
+    }
+
+    @Provides
+    @InternalApi
     @Singleton
-    fun provideRedditService(): RedditService {
+    fun provideOkHttpClient(@InternalApi cache: Cache): OkHttpClient {
         val logTree = Timber.tagged(LOG_TAG)
-        val loggingInterceptor = HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
+        val logger = object : HttpLoggingInterceptor.Logger {
             override fun log(message: String) {
                 logTree.debug { message }
             }
-        }).apply {
+        }
+        val loggingInterceptor = HttpLoggingInterceptor(logger).apply {
             level = HttpLoggingInterceptor.Level.BASIC
         }
-        val okHttpClient = OkHttpClient.Builder()
+        return OkHttpClient.Builder()
+            .cache(cache)
             .addNetworkInterceptor(loggingInterceptor)
             .build()
+    }
 
+    @Provides
+    @Singleton
+    fun provideRedditService(@InternalApi okHttpClient: Lazy<OkHttpClient>): RedditService {
+        val callFactory = object : Call.Factory {
+            override fun newCall(request: Request) = okHttpClient.get().newCall(request)
+        }
         val retrofit = Retrofit.Builder()
-            .client(okHttpClient)
+            .callFactory(callFactory)
             .baseUrl(BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
