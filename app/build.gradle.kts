ca29b7cee93c6cc7d6f0262b3bcc0f4cb5dbec3b
aouerfelli <a.ouerf@gmail.com>
2019-12-29 12:22:20 -0500

    Remove redundant tests (no testing for now)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,8 +99,4 @@ dependencies {
     implementation(Dependencies.SqlDelight.coroutines)
 
     implementation(Dependencies.timber)
-
-    testImplementation("junit:junit:4.12")
-    androidTestImplementation("androidx.test.ext:junit:1.1.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
 }
