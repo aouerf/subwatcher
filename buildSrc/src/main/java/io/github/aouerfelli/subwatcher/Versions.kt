bc623fc2c1c13aace342a40cab7d87a3fa1ce0d4
aouerfelli <a.ouerf@gmail.com>
2020-01-21 18:01:13 -0500

    Update MDC to 1.2.0-alpha04

--- buildSrc/src/main/java/io/github/aouerfelli/subwatcher/Versions.kt
+++ buildSrc/src/main/java/io/github/aouerfelli/subwatcher/Versions.kt
@@ -9,7 +9,7 @@ internal object Versions {
 
   const val sqlDelight = "1.2.1"
 
-  const val material = "1.2.0-alpha03"
+  const val material = "1.2.0-alpha04"
 
   object AndroidX {
     const val appcompat = "1.2.0-alpha01"
