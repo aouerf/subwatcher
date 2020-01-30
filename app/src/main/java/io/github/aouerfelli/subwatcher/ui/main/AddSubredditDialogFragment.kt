41df45ba47701bdcff2aee7a041a8c2209991b3c
aouerfelli <a.ouerf@gmail.com>
2020-01-30 11:57:40 -0500

    Draw the dialog under system bars
    to make it go edge-to-edge.

--- app/src/main/java/io/github/aouerfelli/subwatcher/ui/main/AddSubredditDialogFragment.kt
+++ app/src/main/java/io/github/aouerfelli/subwatcher/ui/main/AddSubredditDialogFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updatePadding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dev.chrisbanes.insetter.doOnApplyWindowInsets
 import io.github.aouerfelli.subwatcher.databinding.AddSubredditDialogFragmentBinding
 
 class AddSubredditDialogFragment : BottomSheetDialogFragment() {
@@ -24,17 +27,44 @@ class AddSubredditDialogFragment : BottomSheetDialogFragment() {
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    val inputFilters = binding?.subredditField?.editText?.filters.orEmpty()
+    val binding = requireNotNull(binding) { "There is no view currently attached to this dialog." }
+    onBindingCreated(binding, savedInstanceState)
+  }
+
+  private fun onBindingCreated(
+    binding: AddSubredditDialogFragmentBinding,
+    savedInstanceState: Bundle?
+  ) {
+    // FIXME: Long-press (text selection toolbar) seems to trigger insets
+    // TODO: Top padding?
+    binding.root.doOnApplyWindowInsets { view, insets, initialState ->
+      view.updatePadding(
+        bottom = insets.systemWindowInsetBottom + initialState.paddings.bottom
+      )
+      view.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+        leftMargin = insets.systemWindowInsetLeft + initialState.margins.left
+        rightMargin = insets.systemWindowInsetRight + initialState.margins.right
+      }
+    }
+
+    val inputFilters = binding.subredditField.editText?.filters.orEmpty()
     val inputWhitespaceFilter = InputFilter { source, _, _, _, _, _ ->
       source.filterNot(Char::isWhitespace)
     }
     // Whitespace filter should come before the maxLength filter so that the character limit doesn't
-    // account for possible whitespace
-    binding?.subredditField?.editText?.filters = arrayOf(inputWhitespaceFilter, *inputFilters)
+    // account for possible whitespace.
+    binding.subredditField.editText?.filters = arrayOf(inputWhitespaceFilter, *inputFilters)
   }
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
     super.onActivityCreated(savedInstanceState)
+
+    // Disable fitsSystemWindow on the dialog container to allow the dialog to be drawn under the
+    // system bars.
+    /*dialog?.window?.findViewById<View>(com.google.android.material.R.id.container)
+      ?.fitsSystemWindows = false*/
+    (view?.parent?.parent?.parent as? View)?.fitsSystemWindows = false
+
     binding?.subredditField?.editText?.requestFocus()
     dialog?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
   }
