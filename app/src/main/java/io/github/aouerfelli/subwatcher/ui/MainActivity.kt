f2ff92315a2d2ddd655125355a0f50df4a63bf03
aouerfelli <a.ouerf@gmail.com>
2020-02-24 10:47:49 -0500

    Enable light navigation bar for API 26

--- app/src/main/java/io/github/aouerfelli/subwatcher/ui/MainActivity.kt
+++ app/src/main/java/io/github/aouerfelli/subwatcher/ui/MainActivity.kt
@@ -1,6 +1,20 @@
 package io.github.aouerfelli.subwatcher.ui
 
+import android.os.Build
+import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import io.github.aouerfelli.subwatcher.R
 
-class MainActivity : AppCompatActivity(R.layout.main_activity)
+class MainActivity : AppCompatActivity(R.layout.main_activity) {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    if (Build.VERSION.SDK_INT == 26 && resources.getBoolean(R.bool.is_light_mode)) {
+      window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or
+          // Light navigation bar must be enabled in code instead of theme for API 26
+          View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+    }
+  }
+}
