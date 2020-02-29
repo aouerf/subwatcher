989f7ce66f5e082916ff93c849966a41f5a3010d
aouerfelli <a.ouerf@gmail.com>
2020-02-29 12:01:25 -0500

    Update AGP to 4.1.0-alpha01

--- buildSrc/src/main/java/io/github/aouerfelli/subwatcher/Versions.kt
+++ buildSrc/src/main/java/io/github/aouerfelli/subwatcher/Versions.kt
@@ -3,7 +3,7 @@ package io.github.aouerfelli.subwatcher
 internal object Versions {
   const val kotlin = "1.3.61"
 
-  const val androidGradlePlugin = "4.0.0-alpha09"
+  const val androidGradlePlugin = "4.1.0-alpha01"
   const val ktlint = "9.2.1"
   const val gradleVersions = "0.28.0"
 
