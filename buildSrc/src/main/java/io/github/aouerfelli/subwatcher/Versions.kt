ccc758f7b0ee26aa377bb0c3c35931bdc533e7fc
aouerfelli <a.ouerf@gmail.com>
2020-03-05 14:20:12 -0500

    Update dependencies

--- buildSrc/src/main/java/io/github/aouerfelli/subwatcher/Versions.kt
+++ buildSrc/src/main/java/io/github/aouerfelli/subwatcher/Versions.kt
@@ -12,18 +12,18 @@ internal object Versions {
   const val androidDesugarJdkLibs = "1.0.4"
 
   object AndroidX {
-    const val appcompat = "1.2.0-alpha02"
-    const val activity = "1.1.0"
-    const val fragment = "1.2.2"
-    const val core = "1.3.0-alpha01"
+    const val appcompat = "1.2.0-alpha03"
+    const val activity = "1.2.0-alpha01"
+    const val fragment = "1.3.0-alpha01"
+    const val core = "1.3.0-alpha02"
     const val constraintLayout = "2.0.0-beta4"
     const val coordinatorLayout = "1.1.0"
     const val recyclerView = "1.2.0-alpha01"
-    const val swipeRefreshLayout = "1.1.0-alpha03"
+    const val swipeRefreshLayout = "1.1.0-beta01"
     const val browser = "1.3.0-alpha01"
 
-    const val lifecycle = "2.2.0"
-    const val work = "2.3.2"
+    const val lifecycle = "2.3.0-alpha01"
+    const val work = "2.4.0-alpha01"
   }
 
   const val material = "1.2.0-alpha05"
