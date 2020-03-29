807772eefd9a10bf01f3736537f8d7824de5e285
aouerfelli <a.ouerf@gmail.com>
2020-03-29 10:53:16 -0400

    Update dependencies

--- buildSrc/src/main/java/com/aouerfelli/subwatcher/Versions.kt
+++ buildSrc/src/main/java/com/aouerfelli/subwatcher/Versions.kt
@@ -2,9 +2,9 @@ package com.aouerfelli.subwatcher
 
 internal object Versions {
 
-  const val kotlin = "1.3.70"
+  const val kotlin = "1.3.71"
 
-  const val androidGradlePlugin = "4.1.0-alpha02"
+  const val androidGradlePlugin = "4.1.0-alpha04"
   const val ktlint = "9.2.1"
   const val gradleVersions = "0.28.0"
 
@@ -14,8 +14,8 @@ internal object Versions {
 
   object AndroidX {
     const val appcompat = "1.2.0-alpha03"
-    const val activity = "1.2.0-alpha01"
-    const val fragment = "1.3.0-alpha01"
+    const val activity = "1.2.0-alpha02"
+    const val fragment = "1.3.0-alpha02"
     const val core = "1.3.0-alpha02"
     const val constraintLayout = "2.0.0-beta4"
     const val coordinatorLayout = "1.1.0"
@@ -31,14 +31,14 @@ internal object Versions {
   const val insetter = "0.2.1"
   const val coil = "0.9.5"
 
-  const val coroutines = "1.3.4"
+  const val coroutines = "1.3.5"
   const val dagger = "2.27"
   const val assistedInject = "0.5.2"
-  const val okHttp = "4.4.1"
-  const val retrofit = "2.7.1"
+  const val okHttp = "4.5.0-RC1"
+  const val retrofit = "2.8.1"
   const val moshi = "1.9.2"
   const val timber = "5.0.0-SNAPSHOT"
 
-  const val jUnit = "5.6.0"
+  const val jUnit = "5.6.1"
   const val mockK = "1.9.3"
 }
