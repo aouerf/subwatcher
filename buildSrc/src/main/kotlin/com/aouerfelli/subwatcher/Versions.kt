68d6b001dfa60df53634e98fa10f37acbfd46972
aouerfelli <ahmad@aouerfelli.com>
2021-01-27 18:55:30 -0500

    Update dependencies

--- buildSrc/src/main/kotlin/com/aouerfelli/subwatcher/Versions.kt
+++ buildSrc/src/main/kotlin/com/aouerfelli/subwatcher/Versions.kt
@@ -2,9 +2,9 @@ package com.aouerfelli.subwatcher
 
 internal object Versions {
 
-  const val kotlin = "1.4.30-M1"
+  const val kotlin = "1.4.30-RC"
 
-  const val androidGradlePlugin = "7.0.0-alpha04"
+  const val androidGradlePlugin = "7.0.0-alpha05"
   const val ktlintGradle = "9.4.1"
   const val gradleVersions = "0.36.0"
 
@@ -15,7 +15,7 @@ internal object Versions {
   object AndroidX {
     const val appcompat = "1.3.0-beta01"
     const val activity = "1.2.0-rc01"
-    const val fragment = "1.3.0-rc01"
+    const val fragment = "1.3.0-rc02"
     const val core = "1.5.0-beta01"
     const val constraintLayout = "2.1.0-alpha2"
     const val coordinatorLayout = "1.1.0"
@@ -24,7 +24,7 @@ internal object Versions {
     const val browser = "1.3.0"
 
     const val lifecycle = "2.3.0-rc01"
-    const val work = "2.5.0-rc01"
+    const val work = "2.5.0"
 
     const val hilt = "1.0.0-alpha03"
   }
@@ -34,7 +34,7 @@ internal object Versions {
   const val coil = "1.1.1"
 
   const val coroutines = "1.4.2"
-  const val dagger = "2.31.1"
+  const val dagger = "2.31.2"
   const val okHttp = "4.10.0-RC1"
   const val retrofit = "2.9.0"
   const val moshi = "1.11.0"
