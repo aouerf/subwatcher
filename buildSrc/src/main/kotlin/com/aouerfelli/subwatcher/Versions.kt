18bf66c99ed290936564d7407c477d6603012802
aouerfelli <ahmad@aouerfelli.com>
2021-02-05 20:23:03 -0500

    Update dependencies

--- buildSrc/src/main/kotlin/com/aouerfelli/subwatcher/Versions.kt
+++ buildSrc/src/main/kotlin/com/aouerfelli/subwatcher/Versions.kt
@@ -2,7 +2,7 @@ package com.aouerfelli.subwatcher
 
 internal object Versions {
 
-  const val kotlin = "1.4.30-RC"
+  const val kotlin = "1.4.30"
 
   const val androidGradlePlugin = "7.0.0-alpha05"
   const val ktlintGradle = "9.4.1"
@@ -29,17 +29,17 @@ internal object Versions {
     const val hilt = "1.0.0-alpha03"
   }
 
-  const val material = "1.3.0-rc01"
+  const val material = "1.3.0"
   const val insetter = "0.4.0"
   const val coil = "1.1.1"
 
   const val coroutines = "1.4.2"
   const val dagger = "2.31.2"
-  const val okHttp = "4.10.0-RC1"
+  const val okHttp = "5.0.0-alpha.2"
   const val retrofit = "2.9.0"
   const val moshi = "1.11.0"
   const val timber = "5.0.0-SNAPSHOT"
 
-  const val jUnit = "5.7.0"
+  const val jUnit = "5.7.1"
   const val mockK = "1.10.5"
 }
